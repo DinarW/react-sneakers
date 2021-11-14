558336b700d57dd163e891c09503432255e47b33
dinar <dinarmkbg@gmail.com>
2021-11-15 01:50:11 +0300

    add back, functions with hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import React from 'react';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 import Card from './components/Card';
 
-const arr = [
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: "12 999", imageUrl: '/img/sneakers/1.jpg' },
-  { title: 'Мужские Кроссовки Nike Air Max 270', price: "12 999", imageUrl: '/img/sneakers/2.jpg' },
-  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: "8 499", imageUrl: '/img/sneakers/3.jpg' },
-  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: "8 999", imageUrl: '/img/sneakers/4.jpg' }
-]
-
  function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+  const [cartItems, setCartItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://61917aef41928b0017690077.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => setItems(json))
+  }, []);
+
+  const onAddToCart = (obj) => {
+    const add = cartItems.filter((porduct) => (porduct.title === obj.title) && (porduct.price === obj.price)).length
+    !add ? setCartItems(prev => [...prev, obj]) : alert('Товар уже добавлен в корзину!');
+  };
+
   return (
     <div className="wrapper clear">
-      <Drawer />
+      {cartOpened && <Drawer items={cartItems} onClose={() => {setCartOpened(false)}} />}
 
-      <Header />
+      <Header onClickCart = {() => setCartOpened(true)} />
 
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
@@ -24,10 +33,17 @@ const arr = [
             <input placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
+        <div className="d-flex flex-wrap">
           {
-            arr.map((curr) => <Card title={curr.title} price={curr.price} imageUrl={curr.imageUrl} />)
-          }
+            items.map((item) => (
+            <Card 
+              title={item.title} 
+              price={item.price} 
+              imageUrl={item.imageUrl} 
+              onFavorite={() => console.log('Добавили в закладки')}
+              onPlus={() => onAddToCart(item)}
+            />
+          ))}
         </div>
       </div>
     </div>
