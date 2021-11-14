3ab0674b832da6e49c126fbe165100232e219db6
dinar <dinarmkbg@gmail.com>
2021-11-14 08:25:42 +0300

    update Cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
- import Header from './components/Header';
- import Drawer from './components/Drawer';
- import Card from './components/Card';
- 
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+import Card from './components/Card';
+
+const arr = [
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: "12 999", imageUrl: '/img/sneakers/1.jpg' },
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: "12 999", imageUrl: '/img/sneakers/2.jpg' },
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: "8 499", imageUrl: '/img/sneakers/3.jpg' },
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: "8 999", imageUrl: '/img/sneakers/4.jpg' }
+]
+
  function App() {
   return (
     <div className="wrapper clear">
@@ -18,10 +25,9 @@
           </div>
         </div>
         <div className="d-flex">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            arr.map((curr) => <Card title={curr.title} price={curr.price} imageUrl={curr.imageUrl} />)
+          }
         </div>
       </div>
     </div>
