cde3bdaf89bcb93c0b9737bce3c3edcef92e2b95
dinar <dinarmkbg@gmail.com>
2021-12-08 14:34:14 +0300

    add react-router, create pages

--- src/App.js
+++ src/App.js
@@ -1,51 +1,84 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import axios from 'axios';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
-import Card from './components/Card';
 
- function App() {
+function App() {
   const [items, setItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
   const [cartOpened, setCartOpened] = React.useState(false);
   const [cartItems, setCartItems] = React.useState([]);
+  const [favorites, setFavorite] = React.useState(false);
 
   React.useEffect(() => {
-    fetch('https://61917aef41928b0017690077.mockapi.io/items')
-      .then((res) => res.json())
-      .then((json) => setItems(json))
+    axios.get('https://61917aef41928b0017690077.mockapi.io/items/').then((res) => {
+      setItems(res.data);
+    });
+    axios.get('https://61917aef41928b0017690077.mockapi.io/cart/').then((res) => {
+      setCartItems(res.data);
+    });
+    axios.get('https://61917aef41928b0017690077.mockapi.io/favorites').then((res) => {
+      setFavorite(res.data);
+    })
   }, []);
 
-  const onAddToCart = (obj) => {
-    const add = cartItems.filter((porduct) => (porduct.title === obj.title) && (porduct.price === obj.price)).length
-    !add ? setCartItems(prev => [...prev, obj]) : alert('Товар уже добавлен в корзину!');
+  const onAddToCart = async (obj) => {
+    await axios.post('https://61917aef41928b0017690077.mockapi.io/cart/', obj);
+    await setCartItems(prev => [...prev, obj]);
   };
+
+  const onRemoveItem = async (id) => {
+    try {
+      await axios.delete(`https://60d62397943aa60017768e77.mockapi.io/cart/:${id}`);
+      await setCartItems((prev) => prev.filter((item) => item.id !== id));
+      await setCartItems((prev) => prev.filter((item) => Number(item.id) !== Number(id)));
+    } 
+    catch (error) {
+      alert('Ошибка при удаления из корзины');
+      console.error(error);
+    }
+  };
+
+  const onAddToFavorite = async (obj) => {
+    try {
+      if (favorites.find((favObj) => favObj.id === obj.id)) {
+        await axios.delete(`/favorites/:${obj.id}`);
+      } else {
+        const { data } = await axios.post('/favorites', obj);
+        setFavorite((prev) => [...prev, data]);
+      }
+    } catch (error) {
+      alert('Не удалось добавить в фавориты');
+    }
+  }
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  }
 
   return (
     <div className="wrapper clear">
-      {cartOpened && <Drawer items={cartItems} onClose={() => {setCartOpened(false)}} />}
+      {cartOpened && <Drawer items={cartItems} onClose={() => {setCartOpened(false)}} onRemove={onRemoveItem} />}
 
       <Header onClickCart = {() => setCartOpened(true)} />
+      <Routes>
+        <Route path="/" exact element={
+          <Home
+            items={items}
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            onChangeSearchInput={onChangeSearchInput}
+            onAddToFavorite={onAddToFavorite}
+            onAddToCart={onAddToCart}/>
+          } />
 
-      <div className="content p-40">
-        <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
-          <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="search" />
-            <input placeholder="Поиск..." />
-          </div>
-        </div>
-        <div className="d-flex flex-wrap">
-          {
-            items.map((item) => (
-            <Card 
-              title={item.title} 
-              price={item.price} 
-              imageUrl={item.imageUrl} 
-              onFavorite={() => console.log('Добавили в закладки')}
-              onPlus={() => onAddToCart(item)}
-            />
-          ))}
-        </div>
-      </div>
+        <Route path="/favorites" exact element={
+          <Favorites items={favorites} onAddToFavorite={onAddToFavorite} />
+        } />
+      </Routes>
     </div>
   );
 }
